3bfa432abd0c6fe58043286bb8128f44dd271b91
9eck0 <36316465+9eck0@users.noreply.github.com>
2025-03-17 14:03:41 -0400

    Added placeholder Panel container

---
+++ pytui/UI/Containers/Panel.py
@@ -0,0 +1,6 @@
+"""
+Panels are a type of container, used to group other components together.
+
+Panels cannot be focused on, explicitly (i.e. to itself) or implicitly via its children components.
+However, focus cycling ordering is impacted by the nesting order of panels.
+"""
